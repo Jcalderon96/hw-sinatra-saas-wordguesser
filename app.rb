6cf8e05bd0a3fd037711d8498c8cf58854357f28
Jim Calderon <jc2455@njit.edu>
2022-11-20 17:15:07 -0500

    app is good

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+
 require 'sinatra/base'
 require 'sinatra/flash'
 require_relative './lib/wordguesser_game.rb'
@@ -6,25 +7,25 @@ class WordGuesserApp < Sinatra::Base
 
   enable :sessions
   register Sinatra::Flash
-  
+
   before do
     @game = session[:game] || WordGuesserGame.new('')
   end
-  
+
   after do
     session[:game] = @game
   end
-  
+
   # These two routes are good examples of Sinatra syntax
   # to help you with the rest of the assignment
   get '/' do
     redirect '/new'
   end
-  
+
   get '/new' do
     erb :new
   end
-  
+
   post '/create' do
     # NOTE: don't change next line - it's needed by autograder!
     word = params[:word] || WordGuesserGame.get_random_word
@@ -33,34 +34,57 @@ class WordGuesserApp < Sinatra::Base
     @game = WordGuesserGame.new(word)
     redirect '/show'
   end
-  
+
   # Use existing methods in WordGuesserGame to process a guess.
   # If a guess is repeated, set flash[:message] to "You have already used that letter."
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
-    ### YOUR CODE HERE ###
+    if @game.guess.include?(letter) or @game.wrong_guesses.include?(letter)
+      flash[:message] = "You have already used that letter."
+    elsif letter == nil or !letter.match?(/[[:alpha:]]/)
+      flash[:message] = "Invalid guess."
+    else
+      @game.guess(letter)
+    end
+    if @game.check_win_or_lose() == :win
+      redirect '/win'
+    elsif @game.check_win_or_lose() == :lose
+      redirect '/lose'
+    end
     redirect '/show'
   end
-  
+
   # Everytime a guess is made, we should eventually end up at this route.
   # Use existing methods in WordGuesserGame to check if player has
   # won, lost, or neither, and take the appropriate action.
   # Notice that the show.erb template expects to use the instance variables
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
-    ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    action = @game.check_win_or_lose
+    case action
+    when :win then redirect '/win'
+    when :lose then redirect '/lose'
+    else erb :show # You may change/remove this line
+    end
   end
-  
+
   get '/win' do
-    ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
+    puts "game #{@game}"
+     action = @game.check_win_or_lose
+      case action
+      when :win then erb :win # You may change/remove this line
+      else redirect '/show'
+      end
+    # You may change/remove this line
   end
-  
+
   get '/lose' do
-    ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    action = @game.check_win_or_lose
+      case action
+      when :lose then erb :lose # You may change/remove this line
+      else redirect '/show'
+      end
   end
-  
+
 end

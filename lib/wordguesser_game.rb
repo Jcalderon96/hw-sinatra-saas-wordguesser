d8f10c4e8870caebb8f8cc6d9a5ce7d8eca76e01
Jim Calderon <jc2455@njit.edu>
2022-11-20 20:17:11 -0500

    again

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -1,18 +1,61 @@
 class WordGuesserGame
 
-  attr_accessor :word
-  attr_accessor :guesses
-  attr_accessor :wrong_guesses
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/wordguesser_game_spec.rb pass.
 
   # Get a word from remote "random word" service
 
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
   def initialize(word)
     @word = word
-    @guesses=""
-    @wrong_guesses=""
+    @guesses = ''
+    @wrong_guesses = ''
   end
+
+  def guess (word)
+    if not ((word =~ /[a-zA-Z]/) == 0 and word.length == 1)
+      raise ArgumentError, 'The input must be a letter'
+    end 
+    right_word = @word.downcase
+    word = word.downcase
+
+    if guesses.include?word or wrong_guesses.include?word
+      return false
+    end
+    
+    if right_word.include?word
+      @guesses.concat(word)
+    else
+      @wrong_guesses.concat(word)
+    end
+    return true
+  end
+
+  def word_with_guesses
+    display = ''
+    @word.each_char do |letter|
+      if @guesses.include?letter
+        display.concat(letter)
+      else
+        display.concat('-')
+      end
+    end
+    return display
+  end
+
+  def check_win_or_lose
+    if @wrong_guesses.length >= 7
+      return :lose
+    end
+    @word.each_char do |letter|
+      if not @guesses.include?letter
+        return :play
+      end
+    end
+    return :win
+  end 
 
   # You can test it by installing irb via $ gem install irb
   # and then running $ irb -I. -r app.rb
@@ -25,56 +68,6 @@ class WordGuesserGame
     Net::HTTP.new('randomword.saasbook.info').start { |http|
       return http.post(uri, "").body
     }
-  end
-
-  # guess函数实现以下功能：
-  # 1.判断输入的字符是否合法
-  # 2.大小写不敏感-》将大写字母转成小写字母
-  # 3.把猜对的字母添加进guesses列表中,猜错的字母添加进wrong_guesses列表中，返回true
-  # 4.重复的字母返回false
-  def guess(ch)
-    unless ch =~ /[a-zA-Z]/
-      raise ArgumentError
-    end
-    ch = ch.downcase
-
-    if @word.include? ch and !(@guesses.include? ch)
-        @guesses << ch
-        true
-    elsif !(@word.include? ch) and !(@wrong_guesses.include? ch)
-      @wrong_guesses << ch
-      true
-    elsif (@guesses.include? ch) or (@wrong_guesses.include? ch)
-      false
-    end
-  end
-
-  # word_with_guesses函数实现以下功能：
-  # 将每一轮的猜词结果存入display_guesses，word中猜对的字母显示，其他字母用-表示
-  def word_with_guesses
-    display_guesses = ""
-    @word.chars do |letter|
-      if @guesses.include? letter
-        display_guesses << letter
-      else
-        display_guesses << '-'
-      end
-    end
-    display_guesses
-  end
-
-  # check_win_or_lose实现以下功能：
-  # 1.全部猜对返回:win
-  # 2.猜错字母数达到7返回:lose
-  # 3.不是1/2继续进行游戏，返回:play
-  def check_win_or_lose
-    if self.word_with_guesses == @word
-      :win
-    elsif @wrong_guesses.length >=7
-      :lose
-    else
-      :play
-    end
   end
 
 end
